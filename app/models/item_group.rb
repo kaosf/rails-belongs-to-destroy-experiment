03f888eab096858055227df59392ec5b99587013
ka <ka.kaosf@gmail.com>
2015-04-11 13:15:19 +0900

    Implement dependent destroy feature

--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -1,2 +1,3 @@
 class ItemGroup < ActiveRecord::Base
+  has_many :items, dependent: :destroy
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "belongs_to destroy" do
+    let!(:item_group) { ItemGroup.create }
+    let!(:items) { item_group.items }
+    let!(:item1) { items.create }
+    let!(:item2) { items.create }
+
+    it { expect(ItemGroup.count).to eq 1 }
+    it { expect(Item.count).to eq 2 }
+    it { expect(items.count).to eq 2 }
+
+    it { expect { item_group.destroy }.to change { ItemGroup.count }.by -1 }
+    it { expect { item_group.destroy }.to change { Item.count }.by -2 }
+
+    it { expect { item1.destroy }.to change { Item.count }.by -1 }
+    it { expect { item1.destroy }.not_to change { ItemGroup.count } }
+
+    it { expect { items.destroy_all }.to change { Item.count }.by -2 }
+    it { expect { items.destroy_all }.to change { Item.count }.to 0 }
+    it { expect { items.destroy_all }.to change { ItemGroup.count }.by -1 }
+  end
 end
